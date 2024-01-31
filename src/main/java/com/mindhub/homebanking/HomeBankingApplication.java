c5030c2f7edf6fada4c38b68c4aa3c6ebdb7826e
Delgaa <112789365+Delgaa@users.noreply.github.com>
2024-01-31 19:14:44 -0300

    cree accounts y los agrege a cada cliente

--- src/main/java/com/mindhub/homebanking/HomeBankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomeBankingApplication.java
@@ -1,11 +1,15 @@
 package com.mindhub.homebanking;
 
+import com.mindhub.homebanking.models.Account;
 import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.repositories.AccountRepository;
 import com.mindhub.homebanking.repositories.ClientRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class HomeBankingApplication {
@@ -15,14 +19,27 @@ public class HomeBankingApplication {
 	}
 
 	@Bean
-	public CommandLineRunner initData(ClientRepository clientRepository){
+	public CommandLineRunner initData(ClientRepository clientRepository, AccountRepository accountRepository){
 		return args -> {
 
-			Client ciente1 = new Client("Melba","Morel","melba@mindhub.com");
-			clientRepository.save(ciente1);
-			System.out.println(ciente1);
+			Client melba = new Client("Melba","Morel","melba@mindhub.com");
+			clientRepository.save(melba);
+			Account accountMelba1 = new Account("VIN001", LocalDate.now(), 5000.00, melba);
+			melba.addAccount(accountMelba1);
+			Account accountMelba2 = new Account("VIN002", LocalDate.now().plusDays(1), 7500.00, melba);
+			melba.addAccount(accountMelba2);
+			accountRepository.saveAll(melba.getAccounts());
+
+			System.out.println(melba);
+
 			Client clientYo = new Client("Ricardo", "Delgado", "delgadoricardoeliseo@gmail.com");
 			clientRepository.save(clientYo);
+			Account accountYo = new Account("VIN003", LocalDate.now(), 7000.00, clientYo);
+			clientYo.addAccount(accountYo);
+			Account accountYo2  = new Account("VIN004", LocalDate.now().plusDays(1), 500.00, clientYo);
+			clientYo.addAccount(accountYo2);
+			accountRepository.saveAll(clientYo.getAccounts());
+
 			System.out.println(clientYo);
 		};
 	}
