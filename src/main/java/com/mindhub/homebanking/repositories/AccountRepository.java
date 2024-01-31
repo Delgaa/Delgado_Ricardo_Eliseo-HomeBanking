94a7c6902c2a28580c6854b1da959e956ff55094
Delgaa <112789365+Delgaa@users.noreply.github.com>
2024-01-31 19:16:16 -0300

    cree AccountRepository y les aplique herencia a JpaRepository

--- src/main/java/com/mindhub/homebanking/repositories/AccountRepository.java
+++ src/main/java/com/mindhub/homebanking/repositories/AccountRepository.java
@@ -1,2 +1,7 @@
-package com.mindhub.homebanking.repositories;public interface AccountRepository {
+package com.mindhub.homebanking.repositories;
+
+import com.mindhub.homebanking.models.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
 }
