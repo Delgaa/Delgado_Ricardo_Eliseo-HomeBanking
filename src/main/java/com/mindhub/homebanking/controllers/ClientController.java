3b1cb9f622167d9b9bde40f09500e829ee2b4734
Delgaa <112789365+Delgaa@users.noreply.github.com>
2024-01-31 19:20:00 -0300

    modifique los metodos getClients y getClientByID con responseEntity

--- src/main/java/com/mindhub/homebanking/controllers/ClientController.java
+++ src/main/java/com/mindhub/homebanking/controllers/ClientController.java
@@ -1,27 +1,38 @@
 package com.mindhub.homebanking.controllers;
 
+import com.mindhub.homebanking.dtos.ClientDTO;
+import java.util.stream.Collectors;
 import com.mindhub.homebanking.models.Client;
 import com.mindhub.homebanking.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/clients")
 public class ClientController {
 
-    @GetMapping("/hello")
-    public String getClients(){
-        return "Hello Clients!";
-    }
     @Autowired
     private ClientRepository clientRepository;
 
     @GetMapping("/")
-    public List<Client> getAllClients(){
-        return clientRepository.findAll();
+    public ResponseEntity<List<ClientDTO>> getClients(){
+        List<Client> clients =clientRepository.findAll();
+        return new ResponseEntity<>(clients.stream().map(ClientDTO::new).collect(Collectors.toList()), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClientDTO> getClientById(@PathVariable Long id){
+        Client client = clientRepository.findById(id).orElse(null);
+        if (client != null){
+            ClientDTO clientDTO = new ClientDTO(client);
+            return new ResponseEntity<>(clientDTO, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 }
