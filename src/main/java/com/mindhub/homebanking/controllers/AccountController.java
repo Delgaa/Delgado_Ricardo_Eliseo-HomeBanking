f3d6308f1a2d7aa6a80a5f499edf308ceeaeac21
Delgaa <112789365+Delgaa@users.noreply.github.com>
2024-02-05 12:24:17 -0300

    modifique ruta de peticion

--- src/main/java/com/mindhub/homebanking/controllers/AccountController.java
+++ src/main/java/com/mindhub/homebanking/controllers/AccountController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/client/accounts")
+@RequestMapping("/api/accounts")
 public class AccountController {
     @Autowired
     private AccountRepository accountRepository;
