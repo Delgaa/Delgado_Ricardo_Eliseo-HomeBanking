8fe5e17a0b25b79078e0c925b90383ca61661bfd
Delgaa <112789365+Delgaa@users.noreply.github.com>
2024-02-04 22:52:16 -0300

    Agregue AccountController con sus mappings

---
+++ src/main/java/com/mindhub/homebanking/controllers/AccountController.java
@@ -0,0 +1,34 @@
+package com.mindhub.homebanking.controllers;
+
+import com.mindhub.homebanking.dtos.AccountDTO;
+import com.mindhub.homebanking.models.Account;
+import com.mindhub.homebanking.repositories.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/client/accounts")
+public class AccountController {
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @GetMapping("/")
+    public ResponseEntity<List<AccountDTO>> getAllAccounts(){
+        List<Account> accounts = accountRepository.findAll();
+        return new ResponseEntity<>(accounts.stream().map(AccountDTO::new).collect(Collectors.toList()), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getAccount(@PathVariable Long id){
+        Account account = accountRepository.findById(id).orElse(null);
+        return account != null ? new ResponseEntity<>(new AccountDTO(account), HttpStatus.OK): ResponseEntity.status(HttpStatus.NOT_FOUND).body("Resource not found");
+    }
+}
