bed472466def5d29dab0159c2785bda85ec50889
Delgaa <112789365+Delgaa@users.noreply.github.com>
2024-02-05 13:47:45 -0300

    Agregue propiedades, metodo constructor, y los metodos getters

--- src/main/java/com/mindhub/homebanking/dtos/TransactionDTO.java
+++ src/main/java/com/mindhub/homebanking/dtos/TransactionDTO.java
@@ -1,4 +1,41 @@
 package com.mindhub.homebanking.dtos;
 
+import com.mindhub.homebanking.models.Transaction;
+import com.mindhub.homebanking.models.TransactionType;
+import java.time.LocalDateTime;
+
 public class TransactionDTO {
+    private Long id;
+    private TransactionType type;
+    private String detail;
+    private LocalDateTime date;
+    private Double amount;
+
+    public TransactionDTO(Transaction transaction) {
+        this.id = transaction.getId();
+        this.type = transaction.getType();
+        this.detail = transaction.getDetail();
+        this.date = transaction.getDate();
+        this.amount = transaction.getAmount();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
 }
