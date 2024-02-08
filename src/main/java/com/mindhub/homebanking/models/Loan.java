491a933b0e7d0c1519eed9ab98c025adde2d1828
Delgaa <112789365+Delgaa@users.noreply.github.com>
2024-02-08 00:08:35 -0300

    cree Loan con sus propiedades y metodos o comportamientos

--- src/main/java/com/mindhub/homebanking/models/Loan.java
+++ src/main/java/com/mindhub/homebanking/models/Loan.java
@@ -1,2 +1,69 @@
-package com.mindhub.homebanking.models;public class Loan {
+package com.mindhub.homebanking.models;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Loan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int maxAmount;
+
+    @ElementCollection
+    private List<Integer> payments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "loan", fetch = FetchType.EAGER)
+    private List<ClientLoan> clients = new ArrayList<>();
+
+    public Loan(String name, int maxAmount, List<Integer> payments) {
+        this.name = name;
+        this.maxAmount = maxAmount;
+        this.payments = payments;
+    }
+
+    public Loan() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMaxAmount() {
+        return maxAmount;
+    }
+
+    public void setMaxAmount(int maxAmount) {
+        this.maxAmount = maxAmount;
+    }
+
+    public List<Integer> getPayments() {
+        return payments;
+    }
+
+    public void setPayments(List<Integer> payments) {
+        this.payments = payments;
+    }
+
+    public List<ClientLoan> getClients() {
+        return clients;
+    }
+
+    public void addClientLoan(ClientLoan clientLoan) {
+        clientLoan.setLoan(this);
+        clients.add(clientLoan);
+    }
 }
