b6c8d49c97342c5d5dc76ba48eea628aad50b107
Delgaa <112789365+Delgaa@users.noreply.github.com>
2024-02-08 00:01:20 -0300

    cree ClientLoanDTO con sus propiedades y comportamientos

--- src/main/java/com/mindhub/homebanking/dtos/ClientLoanDTO.java
+++ src/main/java/com/mindhub/homebanking/dtos/ClientLoanDTO.java
@@ -1,2 +1,44 @@
-package com.mindhub.homebanking.dtos;public class ClientLoanDTO {
+package com.mindhub.homebanking.dtos;
+
+import com.mindhub.homebanking.models.ClientLoan;
+
+public class ClientLoanDTO {
+
+    private Long id;
+
+    private Long loanId;
+
+    private String name;
+
+    private int amount;
+
+    private int payments;
+
+    public ClientLoanDTO(ClientLoan clientLoan) {
+        this.id = clientLoan.getId();
+        this.loanId = clientLoan.getLoan().getId();
+        this.name = clientLoan.getLoan().getName();
+        this.amount = clientLoan.getAmount();
+        this.payments = clientLoan.getPayments();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getLoanId() {
+        return loanId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public int getPayments() {
+        return payments;
+    }
 }
