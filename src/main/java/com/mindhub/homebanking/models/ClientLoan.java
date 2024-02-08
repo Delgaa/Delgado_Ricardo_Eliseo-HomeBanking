fb881aaff400cf02099f26d7b98e796e22b63e6a
Delgaa <112789365+Delgaa@users.noreply.github.com>
2024-02-08 00:05:56 -0300

    cree ClientLoan con sus propiedades y comportamientos o metodos

--- src/main/java/com/mindhub/homebanking/models/ClientLoan.java
+++ src/main/java/com/mindhub/homebanking/models/ClientLoan.java
@@ -1,2 +1,68 @@
-package com.mindhub.homebanking.models;public class ClientLoan {
+package com.mindhub.homebanking.models;
+
+import jakarta.persistence.*;
+
+
+@Entity
+public class ClientLoan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int amount;
+
+    private int payments;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "loan_id")
+    private Loan loan;
+
+    public ClientLoan(int amount, int payments) {
+        this.amount = amount;
+        this.payments = payments;
+    }
+
+    public ClientLoan() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getPayments() {
+        return payments;
+    }
+
+    public void setPayments(int payments) {
+        this.payments = payments;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Loan getLoan() {
+        return loan;
+    }
+
+    public void setLoan(Loan loan) {
+        this.loan = loan;
+    }
 }
