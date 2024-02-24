52ec826954f67010ea98de2250004c51bc51b4af
Delgaa <112789365+Delgaa@users.noreply.github.com>
2024-02-24 18:36:43 -0300

    aplicacion de la configuracion CorsConfig con CorsConfigurationSource

--- src/main/java/com/mindhub/homebanking/configurations/CorsConfig.java
+++ src/main/java/com/mindhub/homebanking/configurations/CorsConfig.java
@@ -1,2 +1,25 @@
-package com.mindhub.homebanking.configurations;public class CorsConfig {
+package com.mindhub.homebanking.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
+
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(Arrays.asList("localhost:5173", "http//localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST","PUT", "PATCH", "DELETE"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
