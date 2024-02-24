10c88425121ca94f93d03870a23d282e89ace2d4
Delgaa <112789365+Delgaa@users.noreply.github.com>
2024-02-24 18:29:19 -0300

    Creacion del filtro jwtRequestFilter que implementa de OncePerRequestFilter y sobrescritura del metodo doFilterInternal

--- src/main/java/com/mindhub/homebanking/filters/JwtRequestFilter.java
+++ src/main/java/com/mindhub/homebanking/filters/JwtRequestFilter.java
@@ -1,2 +1,49 @@
-package com.mindhub.homebanking.filters;public class JwtRequestFilter {
+package com.mindhub.homebanking.filters;
+
+import com.mindhub.homebanking.services.JwtUtilService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtilService jwtUtilService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String authorizationHeader = request.getHeader("Authorization");
+        String email = null;
+        String token = null;
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
+            token = authorizationHeader.substring(7);
+            email = JwtUtilService.extractUsername(token);
+        }
+
+        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(email);
+
+            if (jwtUtilService.validateToken(token, userDetails)){
+                UsernamePasswordAuthenticationToken passwordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+            passwordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(passwordAuthenticationToken);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
 }
