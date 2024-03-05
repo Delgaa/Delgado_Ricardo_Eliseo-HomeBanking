f7c6c8def766aeb57d6a17d71c75f706b322a137
Delgaa <112789365+Delgaa@users.noreply.github.com>
2024-03-05 14:03:13 -0300

    correccion de tipeo en generador de numero de tarjeta

--- src/main/java/com/mindhub/homebanking/utils/GenerateRandomNum.java
+++ src/main/java/com/mindhub/homebanking/utils/GenerateRandomNum.java
@@ -6,7 +6,7 @@ public class GenerateRandomNum {
     }
 
     public String getRandomNumberCard() {
-        return getRandomNumber(1000,10000) + " " + getRandomNumber(1000,10000) + " " + getRandomNumber(1000,10000) + " " + getRandomNumber(1000,10000);
+        return getRandomNumber(1000,10000) + "-" + getRandomNumber(1000,10000) + "-" + getRandomNumber(1000,10000) + "-" + getRandomNumber(1000,10000);
     }
 
     public int getRandomNumberCVV() {
