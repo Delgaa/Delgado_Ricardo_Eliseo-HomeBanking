b7e7e4c98dda2627a3d6db5e3a22c8629545c959
Delgaa <112789365+Delgaa@users.noreply.github.com>
2024-03-07 01:21:14 -0300

    creacion de service CardVerificationService que contendra la logica de negocio CardController

--- src/main/java/com/mindhub/homebanking/controllers/CardController.java
+++ src/main/java/com/mindhub/homebanking/controllers/CardController.java
@@ -1,78 +1,26 @@
 package com.mindhub.homebanking.controllers;
 
 import com.mindhub.homebanking.dtos.CardApplicationDTO;
-import com.mindhub.homebanking.models.Card;
-import com.mindhub.homebanking.models.CardColor;
-import com.mindhub.homebanking.models.CardType;
-import com.mindhub.homebanking.models.Client;
-import com.mindhub.homebanking.services.CardService;
-import com.mindhub.homebanking.services.ClientService;
-import com.mindhub.homebanking.utils.GenerateRandomNum;
+import com.mindhub.homebanking.services.CardVerificationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-import java.time.LocalDate;
-
 
 @RestController
 @RequestMapping("/api/clients/current/cards")
 public class CardController {
 
-    @Autowired
-    private CardService cardService;
 
     @Autowired
-    private ClientService clientService;
+    private CardVerificationService cardVerificationService;
 
     @GetMapping("/")
     public ResponseEntity<?> getAllCards(){
-
-        String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        Client client = clientService.getClientByEmail(email);
-
-        return new ResponseEntity<>(cardService.getCardsDTO(client.getName() + " " + client.getLastName()), HttpStatus.OK);
+        return cardVerificationService.verifyGetAllCards();
     }
 
     @PostMapping("/")
     public ResponseEntity<?> addCard(@RequestBody CardApplicationDTO cardApplicationDTO){
-
-        String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        Client client = clientService.getClientByEmail(email);
-
-        if (cardApplicationDTO.type().isBlank()){
-            return new ResponseEntity<>("Type no content", HttpStatus.BAD_REQUEST);
-        }
-
-        if (cardApplicationDTO.color().isBlank()){
-            return new ResponseEntity<>("Color no content", HttpStatus.BAD_REQUEST);
-        }
-
-        if (cardService.countCardsByTypeAndClient(cardApplicationDTO.type(), client) == 3){
-            return new ResponseEntity<>("The maximum number of" +cardApplicationDTO.type().toLowerCase()+ "cards allowed has been reached", HttpStatus.FORBIDDEN);
-        }
-
-        if (cardService.CardExistsByTypeAndColorAndClient(cardApplicationDTO.type(), cardApplicationDTO.color(), client)){
-            return new ResponseEntity<>("You already have a card of type " + cardApplicationDTO.type().toLowerCase() +" with the color " + cardApplicationDTO.color().toLowerCase(), HttpStatus.FORBIDDEN);
-        }
-
-
-        GenerateRandomNum generateRandomNumCard = new GenerateRandomNum();
-
-        Card newCard = new Card(client,
-                CardType.valueOf(cardApplicationDTO.type()),
-                CardColor.valueOf(cardApplicationDTO.color()),
-                generateRandomNumCard.getRandomNumberCard(),
-                generateRandomNumCard.getRandomNumberCVV(),
-                LocalDate.now().plusYears(5) ,
-                LocalDate.now());
-
-        cardService.saveCard(newCard);
-        client.addCards(newCard);
-        clientService.saveClient(client);
-
-
-        return new ResponseEntity<>("Successfully created", HttpStatus.CREATED);
+        return cardVerificationService.verifyCardApplication(cardApplicationDTO);
     }
 }
--- src/main/java/com/mindhub/homebanking/services/CardVerificationService.java
+++ src/main/java/com/mindhub/homebanking/services/CardVerificationService.java
@@ -1,2 +1,73 @@
-package com.mindhub.homebanking.services;public class CardVerificationService {
+package com.mindhub.homebanking.services;
+
+import com.mindhub.homebanking.dtos.CardApplicationDTO;
+import com.mindhub.homebanking.models.Card;
+import com.mindhub.homebanking.models.CardColor;
+import com.mindhub.homebanking.models.CardType;
+import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.utils.GenerateRandomNum;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import java.time.LocalDate;
+
+@Service
+public class CardVerificationService {
+
+    @Autowired
+    private ClientService clientService;
+
+    @Autowired
+    private CardService cardService;
+
+
+    public ResponseEntity<?> verifyGetAllCards(){
+
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        Client client = clientService.getClientByEmail(email);
+
+        return new ResponseEntity<>(cardService.getCardsDTO(client.getName() + " " + client.getLastName()), HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> verifyCardApplication(CardApplicationDTO cardApplicationDTO) {
+
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        Client client = clientService.getClientByEmail(email);
+
+        if (cardApplicationDTO.type().isBlank()){
+            return new ResponseEntity<>("Type no content", HttpStatus.BAD_REQUEST);
+        }
+
+        if (cardApplicationDTO.color().isBlank()){
+            return new ResponseEntity<>("Color no content", HttpStatus.BAD_REQUEST);
+        }
+
+        if (cardService.countCardsByTypeAndClient(cardApplicationDTO.type(), client) == 3){
+            return new ResponseEntity<>("The maximum number of" +cardApplicationDTO.type().toLowerCase()+ "cards allowed has been reached", HttpStatus.FORBIDDEN);
+        }
+
+        if (cardService.CardExistsByTypeAndColorAndClient(cardApplicationDTO.type(), cardApplicationDTO.color(), client)){
+            return new ResponseEntity<>("You already have a card of type " + cardApplicationDTO.type().toLowerCase() +" with the color " + cardApplicationDTO.color().toLowerCase(), HttpStatus.FORBIDDEN);
+        }
+
+
+        GenerateRandomNum generateRandomNumCard = new GenerateRandomNum();
+
+        Card newCard = new Card(client,
+                CardType.valueOf(cardApplicationDTO.type()),
+                CardColor.valueOf(cardApplicationDTO.color()),
+                generateRandomNumCard.getRandomNumberCard(),
+                generateRandomNumCard.getRandomNumberCVV(),
+                LocalDate.now().plusYears(5) ,
+                LocalDate.now());
+
+        cardService.saveCard(newCard);
+        client.addCards(newCard);
+        clientService.saveClient(client);
+
+
+        return new ResponseEntity<>("Successfully created", HttpStatus.CREATED);
+    }
 }
