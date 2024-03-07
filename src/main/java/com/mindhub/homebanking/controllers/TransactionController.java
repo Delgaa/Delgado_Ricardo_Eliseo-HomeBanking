f49925494a6f8daec6eabaf1abd5ddac5b668959
Delgaa <112789365+Delgaa@users.noreply.github.com>
2024-03-07 01:17:36 -0300

    creacion de service TransactionVerificationService que contendra la logica de negocio TransactionController

--- src/main/java/com/mindhub/homebanking/controllers/TransactionController.java
+++ src/main/java/com/mindhub/homebanking/controllers/TransactionController.java
@@ -1,105 +1,24 @@
 package com.mindhub.homebanking.controllers;
 
 import com.mindhub.homebanking.dtos.TransactionApplicationDTO;
-import com.mindhub.homebanking.models.Account;
-import com.mindhub.homebanking.models.Client;
-import com.mindhub.homebanking.models.Transaction;
-import com.mindhub.homebanking.models.TransactionType;
-import com.mindhub.homebanking.services.AccountService;
-import com.mindhub.homebanking.services.ClientService;
-import com.mindhub.homebanking.services.TransactionService;
-import jakarta.transaction.Transactional;
+import com.mindhub.homebanking.services.TransactionVerificationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/transactions")
 public class TransactionController {
 
     @Autowired
-    private TransactionService transactionService;
+    private TransactionVerificationService transactionVerificationService;
 
-    @Autowired
-    private ClientService clientService;
-
-    @Autowired
-    private AccountService accountService;
-
-    @Transactional
     @PostMapping("/")
     public ResponseEntity<?> addTransaction(@RequestBody TransactionApplicationDTO transactionApplicationDTO){
-
-        String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        Client client = clientService.getClientByEmail(email);
-
-        if (transactionApplicationDTO.numberOrigin().isBlank()){
-            return new ResponseEntity<>("Number account origin is empty",HttpStatus.FORBIDDEN);
-        }
-
-        if (transactionApplicationDTO.numberDestination().isBlank()){
-            return new ResponseEntity<>("Number account destination is empty",HttpStatus.FORBIDDEN);
-        }
-
-        if (transactionApplicationDTO.detail().isBlank()){
-            return new ResponseEntity<>("Description is empty",HttpStatus.FORBIDDEN);
-        }
-
-        if (transactionApplicationDTO.amount() == null){
-            return new ResponseEntity<>("Amount is empty",HttpStatus.FORBIDDEN);
-        }
-
-        if (transactionApplicationDTO.numberOrigin().equals(transactionApplicationDTO.numberDestination())){
-            return new ResponseEntity<>("Account origin is equals account destination",HttpStatus.FORBIDDEN);
-        }
-
-        if (!accountService.accountExistsByNumber(transactionApplicationDTO.numberOrigin())){
-            return new ResponseEntity<>("The account origin not exist", HttpStatus.FORBIDDEN);
-        }
-
-        if (!accountService.accountExistsByNumberAndClient(transactionApplicationDTO.numberOrigin(), client)){
-            return new ResponseEntity<>("The account origin not is your account", HttpStatus.FORBIDDEN);
-        }
-
-        if (!accountService.accountExistsByNumber(transactionApplicationDTO.numberDestination())){
-            return new ResponseEntity<>("The account destination not exist", HttpStatus.FORBIDDEN);
-        }
-
-        Account accountOrigin = accountService.getAccountByNumber(transactionApplicationDTO.numberOrigin());
-
-        if(transactionApplicationDTO.amount() > accountOrigin.getBalance()){
-            return new ResponseEntity<>("The available balance is insufficient", HttpStatus.FORBIDDEN);
-        }
-
-        Transaction DebitTransaction = new Transaction(TransactionType.DEBIT,
-                transactionApplicationDTO.detail() + " to " + transactionApplicationDTO.numberDestination(),
-                LocalDateTime.now(),
-                -transactionApplicationDTO.amount());
-
-        accountOrigin.setBalance((accountOrigin.getBalance() - transactionApplicationDTO.amount()));
-        accountOrigin.addTransaction(DebitTransaction);
-        transactionService.saveTransaction(DebitTransaction);
-        accountService.saveAccount(accountOrigin);
-
-        Account accountDestination = accountService.getAccountByNumber(transactionApplicationDTO.numberDestination());
-
-        Transaction CreditTransaction = new Transaction(TransactionType.CREDIT,
-                transactionApplicationDTO.detail() +" of "+ transactionApplicationDTO.numberOrigin(),
-                LocalDateTime.now(),
-                transactionApplicationDTO.amount());
-
-        accountDestination.setBalance((accountDestination.getBalance() + transactionApplicationDTO.amount()));
-        accountDestination.addTransaction(CreditTransaction);
-        transactionService.saveTransaction(CreditTransaction);
-        accountService.saveAccount(accountDestination );
-
-        return new ResponseEntity<>("Transaction completed successfully", HttpStatus.CREATED);
+       return transactionVerificationService.verifyTransactionApplication(transactionApplicationDTO);
     }
 
 
