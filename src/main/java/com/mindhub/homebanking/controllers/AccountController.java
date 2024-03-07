c08d51e3851d58b2f45897086388fb12c37b1b62
Delgaa <112789365+Delgaa@users.noreply.github.com>
2024-03-07 01:19:00 -0300

    creacion de service AccountVerificationService que contendra la logica de negocio AccountController

--- src/main/java/com/mindhub/homebanking/controllers/AccountController.java
+++ src/main/java/com/mindhub/homebanking/controllers/AccountController.java
@@ -1,17 +1,10 @@
 package com.mindhub.homebanking.controllers;
 
-import com.mindhub.homebanking.dtos.AccountDTO;
-import com.mindhub.homebanking.models.Account;
-import com.mindhub.homebanking.models.Client;
-import com.mindhub.homebanking.services.AccountService;
-import com.mindhub.homebanking.services.ClientService;
-import com.mindhub.homebanking.utils.GenerateRandomNum;
+import com.mindhub.homebanking.services.AccountVerificationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-import java.time.LocalDate;
+
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -19,48 +12,20 @@ import java.time.LocalDate;
 public class AccountController {
 
     @Autowired
-    private AccountService accountService;
-
-    @Autowired
-    private ClientService clientService;
+    private AccountVerificationService accountVerificationService;
 
     @GetMapping("/")
     public ResponseEntity<?> getAllAccounts(){
-        String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        Client client = clientService.getClientByEmail(email);
-
-        return new ResponseEntity<>(accountService.getAccountsDTO(client), HttpStatus.OK);
+        return accountVerificationService.verifyGetAllAccounts();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getAccount(@PathVariable Long id){
-        Account account = accountService.getAccountById(id);
-        return account != null ? new ResponseEntity<>(new AccountDTO(account), HttpStatus.OK): ResponseEntity.status(HttpStatus.NOT_FOUND).body("Resource not found");
+        return accountVerificationService.verifyGetAccount(id);
     }
 
     @PostMapping("/")
     public ResponseEntity<?> addAccount(){
-
-        String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        Client client = clientService.getClientByEmail(email);
-
-        if (client.getAccounts().size() == 3){
-            return new ResponseEntity<>("The maximum of three accounts has already been reached.",HttpStatus.FORBIDDEN);
-        }
-
-        GenerateRandomNum generateRandomNumAccount = new GenerateRandomNum();
-
-        String numNewAccount;
-
-        do{
-            numNewAccount = generateRandomNumAccount.getRandomNumberAccount();
-        }while (accountService.accountExistsByNumber(numNewAccount));
-
-        Account newAccount = new Account(numNewAccount, LocalDate.now(), 0.0);
-        client.addAccount(newAccount);
-        accountService.saveAccount(newAccount);
-        clientService.saveClient(client);
-
-        return new ResponseEntity<>("Successfully created", HttpStatus.CREATED);
+        return accountVerificationService.verifyAccountApplication();
     }
 }
--- src/main/java/com/mindhub/homebanking/services/AccountVerificationService.java
+++ src/main/java/com/mindhub/homebanking/services/AccountVerificationService.java
@@ -1,2 +1,58 @@
-package com.mindhub.homebanking.services;public class AccountVerificationService {
+package com.mindhub.homebanking.services;
+
+import com.mindhub.homebanking.dtos.AccountDTO;
+import com.mindhub.homebanking.models.Account;
+import com.mindhub.homebanking.models.Client;
+import com.mindhub.homebanking.utils.GenerateRandomNum;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import java.time.LocalDate;
+
+@Service
+public class AccountVerificationService {
+
+    @Autowired
+    private AccountService accountService;
+
+    @Autowired
+    private ClientService clientService;
+
+    public ResponseEntity<?> verifyGetAllAccounts(){
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        Client client = clientService.getClientByEmail(email);
+
+        return new ResponseEntity<>(accountService.getAccountsDTO(client), HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> verifyGetAccount(Long id){
+        Account account = accountService.getAccountById(id);
+        return account != null ? new ResponseEntity<>(new AccountDTO(account), HttpStatus.OK): ResponseEntity.status(HttpStatus.NOT_FOUND).body("Resource not found");
+    }
+
+    public ResponseEntity<?> verifyAccountApplication(){
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        Client client = clientService.getClientByEmail(email);
+
+        if (client.getAccounts().size() == 3){
+            return new ResponseEntity<>("The maximum of three accounts has already been reached.", HttpStatus.FORBIDDEN);
+        }
+
+        GenerateRandomNum generateRandomNumAccount = new GenerateRandomNum();
+
+        String numNewAccount;
+
+        do{
+            numNewAccount = generateRandomNumAccount.getRandomNumberAccount();
+        }while (accountService.accountExistsByNumber(numNewAccount));
+
+        Account newAccount = new Account(numNewAccount, LocalDate.now(), 0.0);
+        client.addAccount(newAccount);
+        accountService.saveAccount(newAccount);
+        clientService.saveClient(client);
+
+        return new ResponseEntity<>("Successfully created", HttpStatus.CREATED);
+    }
 }
