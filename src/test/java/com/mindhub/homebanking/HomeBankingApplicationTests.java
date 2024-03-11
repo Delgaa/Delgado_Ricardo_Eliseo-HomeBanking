ce59e9c2ffd9a424fadf1dbed519ef08a4ba3a31
Delgaa <112789365+Delgaa@users.noreply.github.com>
2024-03-11 11:22:29 -0300

    implementacion de tests unitarios a los metodos de GenerateRandomNum

--- src/test/java/com/mindhub/homebanking/HomeBankingApplicationTests.java
+++ src/test/java/com/mindhub/homebanking/HomeBankingApplicationTests.java
@@ -1,7 +1,11 @@
 package com.mindhub.homebanking;
 
+import com.mindhub.homebanking.utils.GenerateRandomNum;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 @SpringBootTest
 class HomeBankingApplicationTests {
@@ -10,4 +14,26 @@ class HomeBankingApplicationTests {
 	void contextLoads() {
 	}
 
+	@RepeatedTest(10)
+	public void testRandomNumberCard() {
+		GenerateRandomNum generateRandomNum = new GenerateRandomNum();
+
+		String card = generateRandomNum.getRandomNumberCard();
+		assertThat(card.length(), equalTo(19));
+	}
+	@RepeatedTest(10)
+	public void testRandomNumberCVV() {
+		GenerateRandomNum generateRandomNum = new GenerateRandomNum();
+
+		String cvv = generateRandomNum.getRandomNumberCVV();
+		assertThat(cvv.length(), equalTo(3));
+	}
+
+	@RepeatedTest(10)
+	public void testRandomNumberAccount() {
+		GenerateRandomNum generateRandomNum = new GenerateRandomNum();
+
+		String account = generateRandomNum.getRandomNumberAccount();
+		assertThat(account.length(), equalTo(12));
+	}
 }
