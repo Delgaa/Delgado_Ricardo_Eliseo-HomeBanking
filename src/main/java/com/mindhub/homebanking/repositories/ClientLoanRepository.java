16a77be20d08fd0f203db7bd69ccad07e0a9c8fa
Delgaa <112789365+Delgaa@users.noreply.github.com>
2024-03-11 11:23:49 -0300

    agregue metodos para los tests

--- src/main/java/com/mindhub/homebanking/repositories/ClientLoanRepository.java
+++ src/main/java/com/mindhub/homebanking/repositories/ClientLoanRepository.java
@@ -5,8 +5,12 @@ import com.mindhub.homebanking.models.ClientLoan;
 import com.mindhub.homebanking.models.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface ClientLoanRepository extends JpaRepository<ClientLoan, Long> {
     Boolean existsLoanByLoanAndClient(Loan loan, Client client);
+
+    List<ClientLoan> findByClient(Client testClient);
+
 }
